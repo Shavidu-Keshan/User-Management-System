975a917c8aa10ab963a5d98a5b003cb24b80186a
Shavidu Keshan <shavidu4321@gmail.com>
2024-08-30 15:32:53 +0530

    create user form file

---
+++ src/UserForm.js
@@ -0,0 +1,16 @@
+const UserForm = props => {
+    <Grid
+            container
+            sx={{
+                backgroundColor: '#ffffff',
+                marginBottom: '30px',
+                display: 'block',
+            }}
+        >
+            <Grid item xs={12}>
+                <Typography component = {'h1'} sx={(color: '#000000')}>User Form</Typography>
+            </Grid>
+        </Grid>
+}
+
+export default UserForm;

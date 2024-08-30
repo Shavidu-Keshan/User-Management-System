49fcf344d916ce2b0f8301eee59c70df0c0af820
Shavidu Keshan <shavidu4321@gmail.com>
2024-08-30 17:05:08 +0530

    create form

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,16 +1,89 @@
-const UserForm = props => {
+import React from 'react';
+import { Button, Grid, Input, Typography } from '@mui/material';
+
+const UserForm = (props) => {
+  return (
     <Grid
-            container
-            sx={{
-                backgroundColor: '#ffffff',
-                marginBottom: '30px',
-                display: 'block',
-            }}
+      container
+      sx={{
+        backgroundColor: "#ffffff",
+        marginBottom: "30px",
+        display: "block",
+      }}
+    >
+      <Grid item xs={12} sm={6} sx={{ display: 'flex', alignItems: 'center' }}>
+        <Typography component={"h1"} sx={{ color: "#000000" }}>
+          User Form
+        </Typography>
+      </Grid>
+      
+      <Grid item xs={12} sm={6} sx={{ display: 'flex', alignItems: 'center' }}>
+        <Typography
+          component={'label'}
+          htmlFor='id'
+          sx={{
+            color: '#000000',
+            marginRight: '20px',
+            fontSize: '16px',
+            width: '100px',
+            display: 'block'
+          }}
         >
-            <Grid item xs={12}>
-                <Typography component = {'h1'} sx={(color: '#000000')}>User Form</Typography>
-            </Grid>
-        </Grid>
-}
+          ID
+        </Typography>
+        <Input
+          type="number"
+          id='id'
+          name='id'
+          sx={{ width: '400px' }}
+          value={''}
+          onChange={e => {}}
+        />
+      </Grid>
+      
+      <Grid item xs={12} sm={6} sx={{ display: 'flex', alignItems: 'center' }}>
+        <Typography
+          component={'label'}
+          htmlFor='name'
+          sx={{
+            color: '#000000',
+            marginRight: '20px',
+            fontSize: '16px',
+            width: '100px',
+            display: 'block'
+          }}
+        >
+          Name
+        </Typography>
+        <Input
+          type="text"
+          id='name'
+          name='name'
+          sx={{ width: '400px' }}
+          value={''}
+          onChange={e => {}}
+        />
+      </Grid>
+      
+      <Button
+        sx={{
+          margin: 'auto',
+          marginBottom: '20px',
+          backgroundColor: '#00c6e6',
+          color: '#000000',
+          marginLeft: '15px',
+          marginTop: '15px',
+          '&:hover': {
+            opacity: '0.7',
+            backgroundColor: '#00c6e6',
+          }
+        }}
+      >
+        Add
+      </Button>
+    </Grid>
+  );
+};
 
 export default UserForm;
+
--- src/Users.js
+++ src/Users.js
@@ -1,4 +1,9 @@
+import UserForm from "./UserForm";
+
 const Users =() => {
+    const User =() => {
+        <UserForm />
+    };
 
 }
 

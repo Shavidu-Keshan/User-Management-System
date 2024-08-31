b4ee11ad05642e21fb88e52ec51846be51c1220d
Shavidu Keshan <shavidu4321@gmail.com>
2024-08-31 08:25:54 +0530

    navigate button users page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import {useNavigate} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import UserForm from './UserForm';
 
+
 function App() {
+  const Navigate =useNavigate();
+
   return (
     <div className="App">
     <header className="App-header">
         <h1>Welcome</h1>
-        <button className='users-button'>User</button>
+        <button className='users-button' onClick={() => Navigate('/users')}>User</button>
         
       </header> 
       
